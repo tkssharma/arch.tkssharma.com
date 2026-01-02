b748879ff4c0da1155a405574cff19638b36ab1d
tkssharma <tarun.softengg@gmail.com>
2026-01-02 19:12:25 +0530

    upgrade

--- gatsby-config.js
+++ gatsby-config.js
@@ -195,6 +195,41 @@ module.exports = {
       },
     },
     // ===================================================================================
+    // Markdown
+    // ===================================================================================
+
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-autolink-headers',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 800,
+              backgroundColor: 'transparent',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+              prompt: {
+                user: 'root',
+                host: 'localhost',
+                global: true,
+              },
+            },
+          },
+        ],
+      },
+    },
+
+    // ===================================================================================
     // Search
     // ===================================================================================
 
